93c628087e46c7bceeab2400caccabafebd04b15
wkwkrnht <wkwkrnht@gmail.com>
2017-06-24 22:36:54 +0900

    fix

--- source/script/native-share.js
+++ source/script/native-share.js
@@ -1,11 +1,9 @@
-function constNativeShare(shareButton){
-    shareButton.addEventListener("click", () => {
-        navigator
-        .share({
-            title: document.title,
-            url: window.location.href
-        })
-        .then(console.log.bind(console))
-        .catch(console.log.bind(console));
-    });
+function doNativeShare(){
+    navigator
+    .share({
+        title: document.title,
+        url: window.location.href
+    })
+    .then(console.log.bind(console))
+    .catch(console.log.bind(console));
 }

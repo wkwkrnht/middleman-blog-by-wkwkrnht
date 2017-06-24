505bdaaac868433841f7ec70832a19d408240467
wkwkrnht <wkwkrnht@gmail.com>
2017-06-24 23:20:20 +0900

    fix

--- source/script/native-share.js
+++
@@ -1,6 +0,0 @@
-function doNativeShare(){
-    var title = document.title;
-    var text = document.getElementsByName('description').item(0).content;
-    var url = window.location.href;
-    navigator.share({title: title, text: text, url: url}).then(() => console.log('Successfully sent share'),error => console.error('Error sharing: ' + error));
-}
